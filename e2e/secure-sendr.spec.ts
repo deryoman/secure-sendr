d2a35456ab2c4e144dd97ac79628eaa8de99980f
Julian Seiss <julian@space22.de>
2023-03-09 14:44:21 +0100

    Add e2e tests

---
+++ e2e/secure-sendr.spec.ts
@@ -0,0 +1,46 @@
+import {test, expect} from '@playwright/test'
+
+test.beforeEach(async ({page}) => {
+    await page.goto('./')
+})
+
+test('Share link should be present', async ({page}) => {
+    await page.fill('textarea[name="payload"]', 'foo bar')
+    await page.click('button[type="submit"]')
+
+    await page.waitForURL(new RegExp('^.*/share/#[a-zA-Z0-9_-]+', 'i'))
+
+    await expect(page.getByRole('textbox'))
+        .toHaveValue(new RegExp('^.*/show#[a-zA-Z0-9_-]+', 'i'))
+})
+
+test('Shared link should be decrypt-able', async ({page}) => {
+    await page.fill('textarea[name="payload"]', 'foo bar')
+    await page.click('button[type="submit"]')
+
+    const shareLink = await page.locator('[data-template-link-input]').inputValue()
+
+    await page.goto(shareLink)
+
+    const decryptedText = await page.locator('[data-template-result-textarea]').inputValue()
+
+    await expect(decryptedText).toBe('foo bar')
+})
+
+test('Shared link should only be able to be used once', async ({page}) => {
+    await page.fill('textarea[name="payload"]', 'foo bar')
+    await page.click('button[type="submit"]')
+
+    const shareLink = await page.locator('[data-template-link-input]').inputValue()
+
+    await page.goto(shareLink)
+
+    const firstDecryptedText = await page.locator('[data-template-result-textarea]').inputValue()
+    await expect(firstDecryptedText).toBe('foo bar')
+
+    await page.goto(shareLink)
+
+    const secondDecryptedText = await page.locator('[data-template-result-textarea]').inputValue()
+    await expect(secondDecryptedText).toBe('')
+})
+
